2fbfc895281545b929fed6843d0c2c19eb4ad809
MayteMejia <maytemejia2504@gmail.com>
2023-11-09 01:10:20 -0400

    "driver local"

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,11 +50,11 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => env('RAILWAY_APP_PATH') . '/public',
-            'url' => env('RAILWAY_APP_URL') . '/storage',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
-        
+                
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
