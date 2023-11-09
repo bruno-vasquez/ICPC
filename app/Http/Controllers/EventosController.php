e40f4f11783b2cddd3d92582fbfc17dd5c7a81fb
bruno-vasquez <brunojoel444@gmail.com>
2023-11-09 11:33:47 -0400

    intento 12481274104

--- app/Http/Controllers/EventosController.php
+++ app/Http/Controllers/EventosController.php
@@ -40,50 +40,49 @@ class EventosController extends Controller
     public function store(Request $request)
     {
         $validation = $request->validate([
-
-            'nombre' => 'required | min:3 | max:30' ,
-            'descripcion' => 'required | min:4 | max:100' ,
-            'fechaIni' => 'required' ,
-            'fechaFin' => 'required' ,
-            'requisitos' => 'required' ,
-            'encargado' => 'required | min:3 | max:40' ,
-            'lugar' => 'required | min:3 | max:20' ,
+            'nombre' => 'required|min:3|max:30',
+            'descripcion' => 'required|min:4|max:100',
+            'fechaIni' => 'required',
+            'fechaFin' => 'required',
+            'requisitos' => 'required',
+            'encargado' => 'required|min:3|max:40',
+            'lugar' => 'required|min:3|max:20',
             'estado' => 'required',
-            'imagen' => 'image | mimes:jpeg, png, jpg, svg'
+            'imagen' => 'image|mimes:jpeg,png,jpg,svg'
         ]);
-        
-        $eventos = new Eventos ();
-        $eventos -> nombre = $request -> nombre;
-        $eventos -> descripcion = $request -> descripcion;
-        $eventos -> fechaIni = $request -> fechaIni;
-        $eventos -> fechaFin = $request -> fechaFin;
-        $eventos -> requisitos = $request -> requisitos;
-        $eventos -> encargado = $request -> encargado;
-        $eventos -> lugar = $request -> lugar;
-        $eventos -> estado = $request -> estado;
 
-        if ($imagen = $request->file('imagen')) {
+        $eventos = new Eventos();
+        $eventos->nombre = $request->nombre;
+        $eventos->descripcion = $request->descripcion;
+        $eventos->fechaIni = $request->fechaIni;
+        $eventos->fechaFin = $request->fechaFin;
+        $eventos->requisitos = $request->requisitos;
+        $eventos->encargado = $request->encargado;
+        $eventos->lugar = $request->lugar;
+        $eventos->estado = $request->estado;
+
+        // Verificar si se proporcionó una imagen
+        if ($request->hasFile('imagen')) {
             $rutaGuardarImg = 'imagen/';
 
-            $imagenEvento = uniqid() . '_' . date('YmdHis') . "." . $imagen->getClientOriginalExtension();
+            $imagenEvento = uniqid() . '_' . date('YmdHis') . "." . $request->file('imagen')->getClientOriginalExtension();
 
-           // try {
-                // Utilizamos el disco "public" de Laravel para almacenar la imagen
-                Storage::disk('public')->putFileAs($rutaGuardarImg, $imagen, $imagenEvento);
+            // Utilizamos el disco "public" de Laravel para almacenar la imagen
+            Storage::disk('public')->putFileAs($rutaGuardarImg, $request->file('imagen'), $imagenEvento);
 
-                // Almacenar solo el nombre del archivo en la base de datos
-                $eventos->imagen = $imagenEvento;
-            /*} catch (\Exception $e) {
-                \Log::error('Error al guardar la imagen: ' . $e->getMessage());
-                return response()->json(['error' => 'Error al guardar la imagen'], 500);
-            }*/
+            // Almacenar solo el nombre del archivo en la base de datos
+            $eventos->imagen = $imagenEvento;
+
+            // Construir la URL completa utilizando asset()
+            $urlImagen = asset("storage/{$rutaGuardarImg}{$imagenEvento}");
         }
-        
-        
-        $eventos -> id_tipoEventos = $request -> id_tipoEventos;
 
-        $eventos -> save();
-        return $eventos;
+        $eventos->id_tipoEventos = $request->id_tipoEventos;
+
+        $eventos->save();
+
+        // Devolver la URL completa si se proporcionó una imagen
+        return response()->json(['eventos' => $eventos, 'urlImagen' => $urlImagen ?? null]);
     }
 
 
@@ -129,16 +128,27 @@ class EventosController extends Controller
         $eventos -> lugar = $request -> lugar;
         $eventos -> estado = $request -> estado;
         $eventos -> id_tipoEventos = $request -> id_tipoEventos;
-        if($imagen = $request->file('imagen'))
-        {
+        if ($request->file('imagen')) {
+            // Eliminamos la imagen anterior (opcional)
+            Storage::disk('public')->delete($eventos->imagen);
+    
+            // Directorio para almacenar las imágenes
             $rutaGuardarImg = 'imagen/';
-            $imagenEvento = date('YmdHis'). "." . $imagen->getClientOriginalExtension();
-            $imagen->move($rutaGuardarImg, $imagenEvento);
-            $competencias['imagen'] = "$imagenEvento";
-        }
-        else
-        {
-            unset($prod['imagen']);
+    
+            // Nombre de la nueva imagen
+            $imagenEvento = date('YmdHis') . "." . $request->file('imagen')->getClientOriginalExtension();
+    
+            try {
+                // Almacenamos la nueva imagen
+                $request->file('imagen')->storeAs($rutaGuardarImg, $imagenEvento, 'public');
+    
+                // Actualizamos el campo de imagen en el modelo
+                $eventos->update(['imagen' => "{$rutaGuardarImg}{$imagenEvento}"]);
+    
+            } catch (\Exception $e) {
+                \Log::error('Error al actualizar la imagen: ' . $e->getMessage());
+                return response()->json(['error' => 'Error al actualizar la imagen'], 500);
+            }
         }
         $eventos -> save();
         return $eventos;
