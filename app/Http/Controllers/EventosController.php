06f0012ac22591a573d321c88dace2de55fdc6bb
bruno-vasquez <brunojoel444@gmail.com>
2023-11-13 01:11:04 -0400

    Corrección modificar imágen

--- app/Http/Controllers/EventosController.php
+++ app/Http/Controllers/EventosController.php
@@ -115,36 +115,63 @@ class EventosController extends Controller
      * @param  \App\Models\Eventos  $eventos
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Eventos $eventos)
-    {
-        $eventos = Eventos::findOrFail ($request->id);
-        $eventos -> nombre = $request -> nombre;
-        $eventos -> descripcion = $request -> descripcion;
-        $eventos -> fechaIni = $request -> fechaIni;
-        $eventos -> fechaFin = $request -> fechaFin;
-        $eventos -> requisitos = $request -> requisitos;
-        $eventos -> encargado = $request -> encargado;
-        $eventos -> lugar = $request -> lugar;
-        $eventos -> estado = $request -> estado;
-        if ($request->hasFile('imagen')) 
-        {
-            $rutaGuardarImg = 'imagen/';
-            $imagenEvento = uniqid() . '_' . date('YmdHis') . "." . $request->file('imagen')->getClientOriginalExtension();
-            Storage::disk('public')->putFileAs($rutaGuardarImg, $request->file('imagen'), $imagenEvento);
     
-            // Construir la URL completa utilizando asset()
-            $urlImagen = asset("storage/{$rutaGuardarImg}{$imagenEvento}");
-    
-            // Asignar la URL completa a la propiedad en el modelo
-            $eventos->imagen = $urlImagen;
-        }
-        $eventos->id_tipoEventos = $request->id_tipoEventos;
-        // Guardar los cambios en la base de datos
-        $eventos->save();
-        // Devolver la respuesta
-        return $eventos; 
-    }
+     public function update(Request $request, Eventos $eventos)
+     {
+         // Validar los datos de la solicitud
+         $validation = $request->validate([
+             'nombre' => 'required|min:3|max:50',
+             'descripcion' => 'required|min:4|max:300',
+             'fechaIni' => 'required',
+             'fechaFin' => 'required',
+             'requisitos' => 'required',
+             'encargado' => 'required|min:3|max:70',
+             'lugar' => 'required|min:3|max:60',
+             'estado' => 'required',
+             'imagen' => 'image|mimes:jpeg,png,jpg,svg'
+         ]);
+     
+         // Buscar el evento por ID
+         $eventos = Eventos::findOrFail($eventos->id);
+     
+         // Actualizar los atributos del evento con los datos de la solicitud
+         $eventos->nombre = $request->nombre;
+         $eventos->descripcion = $request->descripcion;
+         $eventos->fechaIni = $request->fechaIni;
+         $eventos->fechaFin = $request->fechaFin;
+         $eventos->requisitos = $request->requisitos;
+         $eventos->encargado = $request->encargado;
+         $eventos->lugar = $request->lugar;
+         $eventos->estado = $request->estado;
+     
+         // Si se proporciona una nueva imagen, actualizarla
+         if ($request->hasFile('imagen')) {
+            // Eliminar la imagen anterior si existe
+            if ($eventos->imagen) {
+                $rutaImagenAnterior = str_replace(asset('storage/'), 'public/', $eventos->imagen);
+                Storage::disk('public')->delete($rutaImagenAnterior);
+            }
+             $rutaGuardarImg = 'imagen/';
+             $imagenEvento = uniqid() . '_' . date('YmdHis') . "." . $request->file('imagen')->getClientOriginalExtension();
+             Storage::disk('public')->putFileAs($rutaGuardarImg, $request->file('imagen'), $imagenEvento);
+     
+             // Construir la URL completa utilizando asset()
+             $urlImagen = asset("storage/{$rutaGuardarImg}{$imagenEvento}");
+     
+             // Asignar la URL completa a la propiedad en el modelo
+             $eventos->imagen = $urlImagen;
+         }
 
+         // Asignar el id_tipoEventos desde la solicitud
+         $eventos->id_tipoEventos = $request->id_tipoEventos;
+     
+         // Guardar los cambios en la base de datos
+         $eventos->save();
+     
+         // Devolver la respuesta
+         return $eventos;
+     }
+     
     /**
      * Remove the specified resource from storage.
      *
