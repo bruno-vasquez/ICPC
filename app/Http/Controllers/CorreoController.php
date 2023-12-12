8ef4a86293ee60553a126bd11f51e7c480a6ed7f
bruno-vasquez <brunojoel444@gmail.com>
2023-12-11 23:51:47 -0400

    Reporte con nombre del evento o competencia y vista mejorada

--- app/Http/Controllers/CorreoController.php
+++ app/Http/Controllers/CorreoController.php
@@ -21,12 +21,15 @@ class CorreoController extends Controller
         $emailsInteresados = $eventosInteresadosController->getEmailsInteresados($evento_id);
         // Personaliza el contenido del mensaje según tus necesidades
         $contenidoMensaje = $eventos->reporte;
+        $nombre = $eventos->nombre;
+        info($nombre);
 
         // Envía el correo a múltiples destinatarios
         Mail::to($emailsInteresados)
-        ->send(new Reportes($contenidoMensaje));
+        ->send(new Reportes($contenidoMensaje, $nombre, ['subject' => 'Nombre del Evento: ' . $nombre]));
         return "Correo enviado a múltiples destinatarios";
     }
+    
     public function enviarCorreoCompetenciasIndi($competencia_id)
     {
         $competencia = Competencias::find($competencia_id);
@@ -34,10 +37,11 @@ class CorreoController extends Controller
         $emailsParticipantes = $competenciaParticipantes->getEmailsParticipantes($competencia_id);
         // Personaliza el contenido del mensaje según tus necesidades
         $contenidoMensaje = $competencia->reporte;
+        $nombre = $competencia->nombre;
 
         // Envía el correo a múltiples destinatarios
         Mail::to($emailsParticipantes)
-        ->send(new Reportes($contenidoMensaje));
+        ->send(new Reportes($contenidoMensaje, $nombre, ['subject' => 'Nombre de la Competencia: ' . $nombre]));
         return "Correo enviado a múltiples destinatarios";
     }
     public function enviarCorreoCompetenciasGru($competencia_id)
@@ -47,10 +51,11 @@ class CorreoController extends Controller
         $emailsEquipos = $competenciaEquipos->getEmailsEquipos($competencia_id);
         // Personaliza el contenido del mensaje según tus necesidades
         $contenidoMensaje = $competencia->reporte;
-
+        $nombre = $competencia->nombre;
+        
         // Envía el correo a múltiples destinatarios
         Mail::to($emailsEquipos)
-        ->send(new Reportes($contenidoMensaje));
+        ->send(new Reportes($contenidoMensaje, $nombre, ['subject' => 'Nombre de la Competencia: ' . $nombre]));
         return "Correo enviado a múltiples destinatarios";
     }
 }
--- resources/views/emails/multiple_recipients.blade.php
+++ resources/views/emails/multiple_recipients.blade.php
@@ -1,38 +1,67 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Anuncio Importante</title>
-    <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-        }
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>{{$nombre}}</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f4f4f4;
+      margin: 0;
+      padding: 0;
+    }
 
-        .container {
-            max-width: 600px;
-            margin: 20px auto;
-            background-color: #ffffff;
-            padding: 20px;
-            border-radius: 10px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
+    .container {
+      max-width: 600px;
+      margin: 20px auto;
+      background-color: #ffffff;
+      padding: 20px;
+      border-radius: 8px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
 
-        p {
-            font-size: 16px;
-            line-height: 1.6;
-            color: #333333;
-        }
-    </style>
+    h1 {
+      color: #007bff; /* Azul */
+    }
+
+    p {
+      color: #333333; /* Gris oscuro */
+    }
+
+    .cta-button {
+      display: inline-block;
+      padding: 10px 20px;
+      background-color: #007bff;
+      color: #ffffff;
+      text-decoration: none;
+      border-radius: 5px;
+    }
+
+    .footer {
+      margin-top: 20px;
+      text-align: center;
+      color: #777777; /* Gris claro */
+    }
+  </style>
 </head>
 <body>
-    <div class="container">
-        <h2 style="color: #007BFF;">Anuncio Importante</h2>
-        <p>{{ $messageContent }}</p>
+
+  <div class="container">
+    <h1>¡Importante!</h1>
+    <p>Estimado participante,</p>
+    <p>Queremos informarte que ha habido una actualización de información en <b>{{$nombre}}</b>. A continuación, te proporcionamos más detalles:</p>
+
+    <p>{{$reporte}}</p>
+
+    <p>Para obtener más información, sobre otros eventos y competencias por favor visita la página:</p>
+
+    <a href="https://competencias-teal.vercel.app/" class="cta-button">Visitar la página</a>
+    <br>
+    <div class="footer">
+      <p>Gracias por tu atención. Si tienes alguna pregunta, no dudes en ponerte en contacto con nosotros.</p>
     </div>
+  </div>
+
 </body>
 </html>
